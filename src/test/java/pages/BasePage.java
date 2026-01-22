e851a119e3149fb7890dd79fdec27033e2927278
Navroop997 <singhnavroop1@gmail.com>
2026-01-21 23:07:00 -0800

    new updation page factory FindBy

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,15 +19,21 @@ public class BasePage {
     public FluentWait<WebDriver> fluentWait;
     public Actions actions;
 
+
     //Constructor
     public BasePage(WebDriver givenDriver) {
         driver = givenDriver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         actions = new Actions(driver);
+        PageFactory.initElements(driver,this);
     }
 //locator method
     public WebElement findElement(By locator) {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    //element method
+    public WebElement findElement(WebElement element){
+        return wait.until(ExpectedConditions.visibilityOf(element));
     }
 
 }
